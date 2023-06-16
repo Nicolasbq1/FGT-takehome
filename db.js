8c36205579988c240655c51f8a3204c72d25e9c6
Nicolasbq1 <nicolasbq1@gmail.com>
2023-06-16 17:04:54 -0400

    Implement rest of apis

--- db.js
+++ db.js
@@ -25,9 +25,13 @@ class Database{
                 if (err) throw err;
                 var create_tables = 
                     "CREATE TABLE tree_inventory (name VARCHAR(255) NOT NULL PRIMARY KEY, current_inventory INTEGER DEFAULT 0 NOT NULL);"
-                    + "CREATE TABLE purchase_agreements (uuid VARCHAR(255) NOT NULL PRIMARY KEY, quantity_ordered INTEGER NOT NULL, created_datetime DATETIME NOT NULL, tree VARCHAR(255) NOT NULL, FOREIGN KEY (tree) REFERENCES tree_inventory(name));"
-                    + "CREATE TABLE purchase_orders (uuid VARCHAR(255) NOT NULL PRIMARY KEY, quantity INTEGER NOT NULL, created_datetime DATETIME NOT NULL, recieved_datetime DATETIME,parent_pa VARCHAR(255), tree VARCHAR(255) NOT NULL, FOREIGN KEY (tree) REFERENCES tree_inventory(name), FOREIGN KEY (parent_pa) REFERENCES purchase_agreements(uuid));"
-                    + 'INSERT INTO tree_inventory (name,current_inventory) VALUES ("Fir",0)'
+                    + "CREATE TABLE purchase_agreements (uuid VARCHAR(255) NOT NULL PRIMARY KEY, quantity INTEGER NOT NULL, created_datetime DATETIME NOT NULL, tree VARCHAR(255) NOT NULL, FOREIGN KEY (tree) REFERENCES tree_inventory(name));"
+                    + "CREATE TABLE purchase_orders (uuid VARCHAR(255) NOT NULL PRIMARY KEY, quantity INTEGER NOT NULL, created_datetime DATETIME NOT NULL, recieved_datetime DATETIME DEFAULT NULL,parent_pa VARCHAR(255) DEFAULT NULL, tree VARCHAR(255) NOT NULL, FOREIGN KEY (tree) REFERENCES tree_inventory(name), FOREIGN KEY (parent_pa) REFERENCES purchase_agreements(uuid));"
+                    + 'INSERT INTO tree_inventory (name,current_inventory) VALUES ("Fir",1000);'
+                    + `INSERT INTO purchase_agreements (uuid , quantity, created_datetime, tree) values ('TEST_UUID', 10000, '${getCurrentDatetime()}', 'Fir');`
+                    + `INSERT INTO purchase_orders (uuid , quantity, created_datetime, tree) values ('STANDALONE_UUID', 1000, '${getCurrentDatetime()}', 'Fir');`
+                    + `INSERT INTO purchase_orders (uuid , quantity, created_datetime, tree, parent_pa) values ('LINKED_UUID', 1000, '${getCurrentDatetime()}', 'Fir','TEST_UUID');`
+                    + `INSERT INTO purchase_orders (uuid , quantity, created_datetime, recieved_datetime, tree) values ('COMPLETED_PO', 1000, '${getCurrentDatetime()}','${getCurrentDatetime()}', 'Fir');`
                     overridenConnection.query(create_tables, (err,res) => {
                     if (err) throw err;
                     console.log("Tables created");
@@ -44,32 +48,12 @@ class Database{
         });
     }
 
-    getCurrentDatetime(){
-        const date = new Date();
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        const seconds = String(date.getSeconds()).padStart(2, '0');
-
-        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-    }
-
     async awaitedQuery(sql,args){
         return util.promisify(this.connection.query).call(this.connection,sql,args);
     }
 
     async createPurchaseAgreementRow(quantity, tree){
-        let insertionQuery = `INSERT INTO purchase_agreements (uuid , quantity_ordered, created_datetime, tree) values (UUID(), ${quantity}, '${this.getCurrentDatetime()}', '${tree}');`;
-        // const res = await this.connection.query(insertionQuery,(err,res)=>{
-        //     if(err){
-        //         console.log("Error occured");
-        //         throw err.code;
-        //     }
-        //     console.log("Created purchase agreement succesfully!");
-        //     return null;
-        // })
+        let insertionQuery = `INSERT INTO purchase_agreements (uuid , quantity, created_datetime, tree) values (UUID(), ${quantity}, '${getCurrentDatetime()}', '${tree}');`;
         try{
             await this.awaitedQuery(insertionQuery);
             return null;
@@ -80,6 +64,164 @@ class Database{
         }
     }
 
+    async createPurchaseOrderRow(quantity, tree){
+        let insertionQuery = `INSERT INTO purchase_orders (uuid , quantity, created_datetime, tree) values (UUID(), ${quantity}, '${getCurrentDatetime()}', '${tree}');`;
+        try{
+            await this.awaitedQuery(insertionQuery);
+            return null;
+        }
+        catch(error){  
+            console.log("Error!: " + error.code);
+            return error.code;
+        }
+    }
+
+    async createPurchaseOrderRowWithPA(quantity, tree, purchaseAgreementUUID){
+        let insertionQuery = `INSERT INTO purchase_orders (uuid , quantity, created_datetime, tree, parent_pa) values (UUID(), ${quantity}, '${getCurrentDatetime()}', '${tree}', '${purchaseAgreementUUID}');`;
+        try{
+            await this.awaitedQuery(insertionQuery);
+            return null;
+        }
+        catch(error){  
+            console.log("Error!: " + error.code);
+            console.log(error)
+            return error.code;
+        }
+    }
+
+    async verifyPa(purchaseAgreementUUID,tree){
+        let selectionQuery = `SELECT * FROM purchase_agreements where uuid='${purchaseAgreementUUID}';`;
+        try{
+            const res = await this.awaitedQuery(selectionQuery);
+            if(res.length === 0){
+                return {
+                    valid: false,
+                    errorMsg: "Error: Purchase agreement does not exist"
+                }
+            }
+            if(res[0].tree !== tree){
+                return {
+                    valid: false,
+                    errorMsg: "Error: Tree for purchase order does not match parent tree"
+                }
+            }
+            return {
+                valid: true
+            };
+        }
+        catch(error){  
+            console.log("Error!: " + error.code);
+            console.log(error)
+            return {
+                valid: false,
+                errorMsg: "Unknown Error while validating purchase agreement"
+            };
+        }
+    }
+
+    async verifyPo(purchaseOrderUUID){
+        let selectionQuery = `SELECT * FROM purchase_orders where uuid='${purchaseOrderUUID}';`;
+        try{
+            const res = await this.awaitedQuery(selectionQuery);
+            if(res.length === 0){
+                return {
+                    valid: false,
+                    errorMsg: "Error: Purchase order does not exist"
+                }
+            }
+            if(res[0].recieved_datetime){
+                return {
+                    valid: false,
+                    errorMsg: "Error: Purchase order has already been recieved"
+                }
+            }
+            return {
+                valid: true,
+                tree: res[0].tree,
+                quantity: res[0].quantity
+            };
+        }
+        catch(error){  
+            console.log("Error!: " + error.code);
+            console.log(error)
+            return {
+                valid: false,
+                errorMsg: "Unknown Error while validating purchase agreement"
+            };
+        }
+    }
+
+    async confirmReception(purchaseOrderUUID){
+        let updateQuery = `UPDATE purchase_orders SET recieved_datetime='${getCurrentDatetime()}' where uuid='${purchaseOrderUUID}';`;
+        try{
+            const res = await this.awaitedQuery(updateQuery);
+            if(res.length === 0){
+                return {
+                    success: false,
+                    errorMsg: "Error: Failed to update purchase order"
+                }
+            }
+            return {
+                success: true
+            };
+        }
+        catch(error){  
+            console.log("Error!: " + error.code);
+            console.log(error)
+            return {
+                success: false,
+                errorMsg: "Unknown Error while validating purchase agreement"
+            };
+        }
+    }
+
+    async updateTreeInventory(tree,quantity){
+        let getTreeInv = `Select current_inventory FROM tree_inventory where name='${tree}'`
+        try{
+            // TODO: get tree inv and set new inventory in a single db transaction
+            const res = await this.awaitedQuery(getTreeInv);
+            if(res.length === 0){
+                return {
+                    success: false,
+                    errorMsg: "Error: Could not find tree somehow (should be based of a foreign key field"
+                }
+            }
+            const currInventory = res[0].current_inventory;
+
+            let updateQuery = `UPDATE tree_inventory SET current_inventory=${currInventory+quantity} where name='${tree}';`;
+            const treeRes = await this.awaitedQuery(updateQuery);
+            if(treeRes.length === 0){
+                return {
+                    success: false,
+                    errorMsg: "Error: Failed to tree inventory"
+                }
+            }
+            return {
+                success: true
+            };
+        }
+        catch(error){  
+            console.log("Error!: " + error.code);
+            console.log(error)
+            return {
+                success: false,
+                errorMsg: "Unknown Error while validating purchase agreement"
+            };
+        }
+    }
+
+}
+
+function getCurrentDatetime(){
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
 export default new Database()
--- server.js
+++ server.js
@@ -19,21 +19,20 @@ app.get('/', (req, res) => {
  */
 app.post('/create-pa', async (req,res) => {
   if(!req.body.tree || req.body.quantity == null){
-    res.status(400).send({
+    return res.status(400).send({
       error:"ERROR: Did not recieve expected parameters"
     });
   }
   const errors = await Database.createPurchaseAgreementRow(req.body.quantity,req.body.tree);
   if(!errors){
-    res.send(`Successfully created purchase agreement`);
+    return res.send(`Successfully created purchase agreement`);
   }
   if(errors === "ER_NO_REFERENCED_ROW_2"){
-    console.log("err detected")
-    res.status(400).send({
+    return res.status(400).send({
       error:"ERROR: Invalid tree entered"
     });
   }
-  res.status(400).send({
+  return res.status(400).send({
     error:"ERROR: Unknown server error"
   });
 });
@@ -44,8 +43,24 @@ app.post('/create-pa', async (req,res) => {
  *    quantity: number of trees promised over course of pa
  *    tree: what tree is being ordered
  */
-app.post('/create-po',(req,res)=>{
-
+app.post('/create-po',async (req,res)=>{
+  if(!req.body.tree || req.body.quantity == null){
+    return res.status(400).send({
+      error:"ERROR: Did not recieve expected parameters"
+    });
+  }
+  const errors = await Database.createPurchaseOrderRow(req.body.quantity,req.body.tree);
+  if(!errors){
+    return res.send(`Successfully created purchase order`);
+  }
+  if(errors === "ER_NO_REFERENCED_ROW_2"){
+    return res.status(400).send({
+      error:"ERROR: Invalid tree entered"
+    });
+  }
+  return res.status(400).send({
+    error:"ERROR: Unknown server error"
+  });
 });
 
 /**
@@ -55,27 +70,74 @@ app.post('/create-po',(req,res)=>{
  *    tree: what tree is being ordered
  *    pa: uuid of pa this po is being referenced to
  */
-app.post('/create-po-under-pa',(req,res)=>{
+app.post('/create-po-under-pa',async (req,res)=>{
+  if(!req.body.tree || req.body.quantity == null || !req.body.pa){
+    return res.status(400).send({
+      error:"ERROR: Did not recieve expected parameters"
+    });
+  }
+  const verifyPa = await Database.verifyPa(req.body.pa,req.body.tree);
+  if(!verifyPa.valid){
+    return res.status(400).send({
+      error:verifyPa.errorMsg
+    });
+  }
 
+  const errors = await Database.createPurchaseOrderRowWithPA(req.body.quantity,req.body.tree,req.body.pa);
+  if(!errors){
+    return res.send(`Successfully created purchase order under pa ` + req.body.pa);
+  }
+  return res.status(400).send({
+    error:"ERROR: Unknown server error"
+  });
 });
+
+
+const recievePoHandler =  async (req,res)=>{
+  if(!req.body.po){
+    return res.status(400).send({
+      error:"ERROR: Did not recieve expected parameters"
+    });
+  }
+  const verifyPo = await Database.verifyPo(req.body.po);
+  if(!verifyPo.valid){
+    return res.status(400).send({
+      error:verifyPo.errorMsg
+    });
+  }
+
+  const confirm = await Database.confirmReception(req.body.po);
+  if(!confirm.success){
+    return res.status(400).send({
+      error:confirm.errorMsg
+    });
+  }
+
+  const updated = await Database.updateTreeInventory(verifyPo.tree,verifyPo.quantity);
+  if(!updated.success){
+    return res.status(400).send({
+      error:updated.errorMsg
+    });
+  }
+
+  return res.send("Sucessfully recieved PO");
+}
 
 /**
  * Recieves a standalone purchase order
  * Expected body:
  *   po: uuid of po that is being recieved
  */
-app.put('/recieve-po',(req,res)=>{
-
-});
+app.put('/recieve-po', recievePoHandler);
 
 /**
  * Recieves a purchase order that should be under a referenced purchase agreement
  * Expected body:
  *   po: uuid of po that is being recieved (this po will need to be linked to a pa)
  */
-app.put('/recieve-po-under-pa',(req,res)=>{
+app.put('/recieve-po-under-pa', recievePoHandler);
 
-});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
