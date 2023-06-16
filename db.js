6641bb0bd63dee0ee71638a1a790b838de901866
Nicolasbq1 <nicolasbq1@gmail.com>
2023-06-16 01:48:11 -0400

    Created main frame of endpoints that will be managing database

--- db.js
+++ db.js
@@ -11,6 +11,38 @@ class Database{
           });
     }
 
+    createAllTables(){
+        this.connection.query("CREATE DATABASE fgt_db", (err,res) => {
+            if (err) throw err;
+            this.connection = mysql.createConnection({
+                ...config,
+                multipleStatements: true,
+                database: "fgt_db"
+            });
+            const overridenConnection = this.connection;
+            overridenConnection.connect(function(err) {
+                if (err) throw err;
+                var create_tables = 
+                    "CREATE TABLE tree_inventory (name VARCHAR(255) NOT NULL PRIMARY KEY, current_inventory INTEGER DEFAULT 0 NOT NULL);"
+                    + "CREATE TABLE purchase_agreements (uuid BINARY(16) NOT NULL PRIMARY KEY, quantity_ordered INTEGER NOT NULL, created_datetime DATETIME NOT NULL, tree VARCHAR(255) NOT NULL REFERENCES tree_inventory (name));"
+                    + "CREATE TABLE purchase_orders (uuid BINARY(16) NOT NULL PRIMARY KEY, quantity INTEGER NOT NULL, created_datetime DATETIME NOT NULL, recieved_datetime DATETIME,parent_pa BINARY(16) REFERENCES purchase_agreements (uuid), tree VARCHAR(255) NOT NULL REFERENCES tree_inventory (name));"
+                    + 'INSERT INTO tree_inventory (name,current_inventory) VALUES ("Fir",0)'
+                    overridenConnection.query(create_tables, (err,res) => {
+                    if (err) throw err;
+                    console.log("Tables created");
+                });
+            });
+        });
+    }
+
+    // PURELY FOR TESTING PURPOSES WOULD NOT EXIST ON ANY PRODUCTION CODE
+    tearDownDatabase(){
+        this.connection.query("DROP DATABASE IF EXISTS fgt_db",(err,res) => {
+            if (err) throw err;
+            console.log("Succsefully dropped fgt_db if it existed");
+        });
+    }
+
 }
 
 export default new Database()
--- server.js
+++ server.js
@@ -1,14 +1,67 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import Database from './db.js';
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+/**
+ * Creates a purchase agreement that needs to be filled by purchase orders
+ * Expected body:
+ *    quantity: number of trees promised over course of pa
+ *    tree: what tree is being ordered
+ */
+app.post('/create-pa',function(req,res){
+  res.send();
+});
+
+/**
+ * Creates a standalone purchase order
+ * Expected body:
+ *    quantity: number of trees promised over course of pa
+ *    tree: what tree is being ordered
+ */
+app.post('/create-po',(req,res)=>{
+
+});
+
+/**
+ * Creates a purchase order that is fullfilling a referenced purchase agreement
+ * Expected body:
+ *    quantity: number of trees promised over course of pa
+ *    tree: what tree is being ordered
+ *    pa: uuid of pa this po is being referenced to
+ */
+app.post('/create-po-under-pa',(req,res)=>{
+
+});
+
+/**
+ * Recieves a standalone purchase order
+ * Expected body:
+ *   po: uuid of po that is being recieved
+ */
+app.put('/recieve-po',(req,res)=>{
+
+});
+
+/**
+ * Recieves a purchase order that should be under a referenced purchase agreement
+ * Expected body:
+ *   po: uuid of po that is being recieved (this po will need to be linked to a pa)
+ */
+app.put('/recieve-po-under-pa',(req,res)=>{
+
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-  
+  Database.tearDownDatabase();
+  Database.createAllTables();
 });
